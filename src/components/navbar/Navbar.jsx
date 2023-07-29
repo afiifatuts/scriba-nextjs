8606fb5470d6b051b49517ae68c160bd2caebc55
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-29 10:50:57 +0700

    [feature] add dark&light mode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from './page.module.css'
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle'
 
 const links =[
   {
@@ -34,7 +35,9 @@ function Navbar() {
   return (
     <div className={styles.container}>
         <Link href= "/" className={styles.logo}>scriba</Link>
+
         <div className={styles.links}>
+          <DarkModeToggle/>
           {links.map(link=>(
             <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
           ))}
